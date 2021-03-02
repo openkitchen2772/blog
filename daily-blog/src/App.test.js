0e9ca49c3aa1401407dca753c1517778bd5cdfd5
openkitchen2772 <william27722016@gmail.com>
2021-03-03 03:26:05 +0800

    test (blog): add initial test suite

---
+++ daily-blog/src/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+import App from './App';
+
+configure({ adapter: new Adapter() })
+
+describe("<App />", () => {
+    it('should render a <App /> as root element', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.hasClass("App")).toEqual(true);
+    });
+})
